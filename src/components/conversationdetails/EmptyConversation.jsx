9e304bc208d1947b62895ecdaa45491e10ab2d1e
Carlos Gozalbes <carlosfgmendoza@gmail.com>
2022-03-30 17:32:51 +0200

    emptyconversation Component

--- src/components/conversationdetails/EmptyConversation.jsx
+++ src/components/conversationdetails/EmptyConversation.jsx
@@ -5,7 +5,7 @@ import { Image } from 'react-bootstrap'
 export default function EmptyConversation() {
   return (
     <div>
-    <Image src={whatsappbackground} className='no_conversation_background' style={{height:'100vh',width:'100%',borderBottom:'3px solid green'}} />
+    <Image src={whatsappbackground} className='no_conversation_background' style={{height:'100vh',width:'100%',borderBottom:'3px solid green',objectFit:'cover'}} />
     </div>
   )
 }
